2ef2b529faae64d7195c31b62c230d36c2b3f287
subramanyayash <subramanya.star@gmail.com>
2015-06-10 17:57:32 +0530

    notification

--- www/js/signout.js
+++ www/js/signout.js
@@ -1,19 +1,27 @@
 ﻿$(function () {
-    $("#signout").click(function () {
-        navigator.notification.confirm(
-  'Are you sure you want to logout?',
-  function (button) {
-      if (button == "1" || button == 1) {
-          window.localStorage.removeItem("username");
-          window.localStorage.removeItem("password");
-          window.location = "index.html";
-      }
-      // alert( 'Success: ' + button );
-  },
-  'Asset Track',
-  'Yes,No'
-);
 
+
+    function onConfirm(buttonIndex) {
+        //alert('You selected button ' + buttonIndex);
+        if (buttonIndex == 1) {
+            window.localStorage.removeItem("username");
+            window.localStorage.removeItem("password");
+            window.location = "index.html";
+        }
+    }
+
+    // Show a custom confirmation dialog
+    //
+    function showConfirm() {
+        navigator.notification.confirm(
+            'Do you want to logout!', // message
+             onConfirm,            // callback to invoke with index of button pressed
+            'AsseTrack',           // title
+            ['Yes', 'No']         // buttonLabels
+        );
+    }
+    $("#signout").click(function () {
+        showConfirm();
     });
 
 });
