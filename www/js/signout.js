486d5272b8e7e7b6cf5b4f442f65ee3994cc58ff
subramanyayash <subramanya.star@gmail.com>
2015-06-10 15:58:03 +0530

    included signout function

---
+++ www/js/signout.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $("#signout").click(function () {
+        window.localStorage.removeItem("username");
+        window.localStorage.removeItem("password");
+        window.location = "index.html";
+    });
+
+});
