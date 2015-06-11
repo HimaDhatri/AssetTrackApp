61dcc7a4853f4cb5136a76b3b75dd3f70377d595
subramanyayash <subramanya.star@gmail.com>
2015-06-11 11:31:10 +0530

    back button

--- www/js/index.js
+++ www/js/index.js
@@ -27,14 +27,15 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener("backbutton", onBackKeyDown, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        //app.receivedEvent('deviceready');
-        document.addEventListener("backbutton", onBackKeyDown, false);        
+        app.receivedEvent('deviceready');
+             
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
