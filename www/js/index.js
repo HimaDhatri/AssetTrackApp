6b9b986e657d31d381f2cd345d96aafbcac78806
subramanyayash <subramanya.star@gmail.com>
2015-06-11 10:24:43 +0530

    test

--- www/js/index.js
+++ www/js/index.js
@@ -63,19 +63,17 @@ var app = {
     }
 };
 function onBackKeyDown() {
-
+    alert("hello");
     navigator.notification.confirm(
             'Do you want to exit!', // message
-             onConfirm,            // callback to invoke with index of button pressed
+             onConfirmation,            // callback to invoke with index of button pressed
             'AsseTrack',           // title
             ['Yes', 'No']         // buttonLabels
         );
 }
 
-
-
-function onConfirm(buttonIndex) {
-    //alert('You selected button ' + buttonIndex);
+function onConfirmation(buttonIndex) {
+    alert('You selected button ' + buttonIndex);
     if (buttonIndex == 1) {
         navigator.app.exitApp();
     }
