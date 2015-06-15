8b9eb21c88a11b1904d322f47fab3eb698fed75d
HimaDhatri <hima@star-knowledge.in>
2015-06-15 19:17:11 +0530

    test for back button

---
+++ www/js/dashboard.js
@@ -0,0 +1,20 @@
+﻿function bindEvents() {
+    
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+function onBackKeyDown() {
+   
+    navigator.notification.confirm(
+            'Do you want to exit!', // message
+             onConfirm,            // callback to invoke with index of button pressed
+            'AsseTrack',           // title
+            ['Yes', 'No']         // buttonLabels
+        );
+}
+
+function onConfirm(buttonIndex) {
+    //alert('You selected button ' + buttonIndex);
+    if (buttonIndex == 1) {
+        navigator.app.exitApp();
+    }
+}
